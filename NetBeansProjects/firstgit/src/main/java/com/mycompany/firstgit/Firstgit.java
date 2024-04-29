6148a41eacd5f4fc3995253f38ceaf396c40204d
Fatso <220697310@ump.ac.za>
2024-04-29 12:33:00 +0200

    git first commit

---
+++ NetBeansProjects/firstgit/src/main/java/com/mycompany/firstgit/Firstgit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstgit;
+
+/**
+ *
+ * @author Tshegofatso Mashale
+ */
+public class Firstgit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
