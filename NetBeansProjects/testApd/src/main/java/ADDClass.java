280033835e7813e3f99018e9b8f7c7517351f699
Fatso <220697310@ump.ac.za>
2024-05-07 16:26:45 +0200

    git first commit

--- NetBeansProjects/testApd/src/main/java/ADDClass.java
+++ NetBeansProjects/testApd/src/main/java/ADDClass.java
@@ -8,5 +8,12 @@
  * @author Tshegofatso Mashale
  */
 public class ADDClass {
-    
+    public class TshegofatsoMashale220697310 {
+     public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
 }
