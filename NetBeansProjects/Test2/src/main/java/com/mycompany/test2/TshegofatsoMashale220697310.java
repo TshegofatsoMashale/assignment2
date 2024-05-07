4f17a87afc8f90d270842391f4e503f7d26e51c9
Fatso <220697310@ump.ac.za>
2024-05-07 15:02:58 +0200

    git first commit

---
+++ NetBeansProjects/Test2/src/main/java/com/mycompany/test2/TshegofatsoMashale220697310.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test2;
+
+/**
+ *
+ * @author Tshegofatso Mashale
+ */
+public class TshegofatsoMashale220697310 {
+     public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
